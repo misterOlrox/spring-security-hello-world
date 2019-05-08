b60ab604000cd6b4a5404063ac50a1276d5176d1
Nikita <nikitakholdobin@yandex.ru>
2019-05-09 00:33:56 +0300

    Add database with plain text passwords

--- src/main/java/com/olrox/security/demo/config/DemoAppConfig.java
+++ src/main/java/com/olrox/security/demo/config/DemoAppConfig.java
@@ -1,16 +1,31 @@
 package com.olrox.security.demo.config;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.util.logging.Logger;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.olrox.security.demo")
+@PropertySource("classpath:persistance-mysql.properties")
 public class DemoAppConfig {
+
+    // will hold data read from properties files
+    @Autowired
+    private Environment env;
+
+    private Logger logger = Logger.getLogger(getClass().getName());
 
     @Bean
     public ViewResolver viewResolver(){
@@ -21,5 +36,51 @@ public class DemoAppConfig {
         viewResolver.setSuffix(".jsp");
 
         return viewResolver;
+    }
+
+    // define a bean for our security datasource
+    @Bean
+    public DataSource securityDataSource(){
+
+        // create connection pool
+        ComboPooledDataSource securityDataSource =
+                new ComboPooledDataSource();
+
+        // set the jdbc driver class
+        try {
+            securityDataSource.setDriverClass(env.getProperty("jdbc.driver"));
+        } catch (PropertyVetoException e) {
+            throw new RuntimeException(e);
+        }
+
+        // log the connection props
+        logger.info(">>> jdbc.url=" + env.getProperty("jdbc.url"));
+        logger.info(">>> jdbc.user=" + env.getProperty("jdbc.user"));
+
+        // set database connection props
+        securityDataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+        securityDataSource.setUser(env.getProperty("jdbc.user"));
+        securityDataSource.setPassword(env.getProperty("jdbc.password"));
+
+        // set connection pool props
+        securityDataSource.setInitialPoolSize(
+                getIntProperty("connection.pool.initialPoolSize"));
+
+        securityDataSource.setMinPoolSize(
+                getIntProperty("connection.pool.minPoolSize"));
+
+        securityDataSource.setMaxPoolSize(
+                getIntProperty("connection.pool.maxPoolSize"));
+
+        securityDataSource.setMaxIdleTime(
+                getIntProperty("connection.pool.maxIdleTime"));
+
+        return securityDataSource;
+    }
+
+    private int getIntProperty(String propName){
+        String propVal = env.getProperty(propName);
+
+        return Integer.parseInt(propVal);
     }
 }
--- src/main/java/com/olrox/security/demo/config/DemoSecurityConfig.java
+++ src/main/java/com/olrox/security/demo/config/DemoSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.olrox.security.demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,19 +8,19 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private DataSource securityDataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        User.UserBuilder users = User.withDefaultPasswordEncoder();
-
-        auth.inMemoryAuthentication()
-                .withUser(users.username("John").password("test123").roles("EMPLOYEE"))
-                .withUser(users.username("Mary").password("test123").roles("MANAGER", "EMPLOYEE"))
-                .withUser(users.username("Susan").password("test123").roles("ADMIN", "EMPLOYEE"));
-
+        // use jdbc authentication ... oh yeah!!!
+        auth.jdbcAuthentication().dataSource(securityDataSource);
     }
 
     @Override
