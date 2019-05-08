8c9329882b7014ed4e1e5083884282fd37b17a2c
Nikita <nikitakholdobin@yandex.ru>
2019-05-08 21:09:34 +0300

    Add test security

---
+++ src/main/java/com/olrox/security/demo/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.olrox.security.demo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
