31570ee600a582b0b65955945d271a7bebc0eb36
Nikita <nikitakholdobin@yandex.ru>
2019-05-08 23:30:06 +0300

    Add access-denied page

--- src/main/java/com/olrox/security/demo/controller/LoginController.java
+++ src/main/java/com/olrox/security/demo/controller/LoginController.java
@@ -5,10 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
-
     @GetMapping("/showMyLoginPage")
     public String showMyLoginPage(){
-
         return "fancy-login";
+    }
+
+    @GetMapping("/access-denied")
+    public String showAccessDenied(){
+        return "access-denied";
     }
 }
---
+++ src/main/webapp/WEB-INF/view/access-denied.jsp
@@ -0,0 +1,21 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+<html>
+
+<head>
+
+    <title>Error - Access Denied</title>
+
+</head>
+
+<body>
+
+    <h2>Access Denied - You are not authorized to access this resource</h2>
+
+    <hr>
+
+    <a href="${pageContext.request.contextPath}/" >Back to homepage</a>
+
+</body>
+
+</html>
